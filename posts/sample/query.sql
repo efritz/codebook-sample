477ec8e81b33067864b142a540a3f7ee3ac3f0d1
Eric Fritz <eric@eric-fritz.com>
2021-01-26 20:16:52 -0600

    Version B

--- posts/sample/query.sql
+++ posts/sample/query.sql
@@ -1,24 +1,22 @@
-WITH RECURSIVE lineage(repository, "commit", parent_commit, has_lsif_data, distance, direction) AS (
+WITH RECURSIVE lineage(repository, "commit", parent_commit, has_lsif_data, direction) AS (
     -- seed result set with the target repository and commit marked
     -- with both ancestor and descendant directions
     SELECT l.* FROM (
-        SELECT c.*, 0, 'A' FROM commits_with_lsif_data_markers c UNION
-        SELECT c.*, 0, 'D' FROM commits_with_lsif_data_markers c
+        SELECT c.*, 'A' FROM commits_with_lsif_data_markers c UNION
+        SELECT c.*, 'D' FROM commits_with_lsif_data_markers c
     ) l
     WHERE l.repository = $1 AND l."commit" = $2
 
     UNION
 
     -- get the next commit in the ancestor or descendant direction
-    SELECT c.*, l.distance + 1, l.direction FROM lineage l
+    SELECT c.*, l.direction FROM lineage l
     JOIN commits_with_lsif_data_markers c ON (
         (l.direction = 'A' AND c.repository = l.repository AND c."commit" = l.parent_commit) or
         (l.direction = 'D' AND c.repository = l.repository AND c.parent_commit = l."commit")
     )
-    -- limit traversal distance
-    WHERE l.distance < $3
 )
 
 -- lineage is ordered by distance to the target commit by
 -- construction; get the nearest commit that has LSIF data
-SELECT l."commit" FROM lineage l WHERE l.has_lsif_data LIMIT 1;
+SELECT l."commit" FROM (SELECT * FROM lineage WHERE l.distance < $3) l WHERE l.has_lsif_data LIMIT 1;
